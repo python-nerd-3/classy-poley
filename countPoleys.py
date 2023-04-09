51fa04af090ae60872a959e5be962a3002dd6feb
python-nerd-3 <117922845+python-nerd-3@users.noreply.github.com>
2023-04-09 08:24:12 -0400

    CLASSY POLEY RELEASE classy poley is finally out

---
+++ countPoleys.py
@@ -0,0 +1,2 @@
+import os
+print("Amount of poleys: " + len(os.listdir('./poleys/')))/2 + " (If the amount is a decimal, there is a missing shiny)"
--- game.js
+++ game.js
@@ -1,213 +1,180 @@
-let notYetPoleys = [ "mrPoley", "jacketPoley", "gumPoley", 
-"toastPoley", "poleys", "poleytrio", "poleyduo", "poleyard", "lorePoley", 
-"boliePoley"]
-let muted = false;
+let totalRarity = 100
+let poleysObj = {}
 
-// Poleys
+let catchingMusic = new Audio("./catching.mp3")
+catchingMusic.volume = 0.5
+catchingMusic.loop = true
+let throwSfx = new Audio("./throw.mp3")
+throwSfx.volume = 0.7
+let shinySfx = new Audio("./shinyPoley.mp3")
+
+function playsfx() {
+    if (this.paused) {
+        this.play()
+    } else {
+        this.currentTime = 0;
+    }
+}
+
+function objectMap(obj, func) { 
+    return Object.fromEntries(Object.entries(obj).map(([k, v]) => [k, func(v)]));
+} // this is from stack overflow
+
+function roundBy(num, places) {
+    return Math.round(num * 10**places) / 10**places
+}
+
+
+Audio.prototype.playsfx = playsfx // and this is from my other project
+
 class Poley {
-    constructor(name, rarity) {
+    constructor(name, display, rarity) {
         this.name = name;
         this.rarity = rarity;
+        this.display = display;
+        this.shinyDisplay = "Shiny " + display;
+        this.shinyRarity = rarity / 100;
+        this.amt = 0;
+        this.shinyAmt = 0;
+        this.percentChunk = [totalRarity, totalRarity - rarity]
+        this.shinyPercentChunk =[this.percentChunk[0], this.percentChunk[0] - this.shinyRarity] 
+        totalRarity -= this.rarity
+        poleysObj[name] = this
+        this.texturePath = `./poleys/${name}.png`
+        this.shinyTexturePath = `./poleys/shiny${name}.png`
     }
 }
 
-let poley = new Poley("poley", 59.4);
-let shiny_poley = new Poley("shiny poley", 0.6);
-let poleyCounter = 0;
-let shinyPoleyCounter = 0;
+// define in alphabetical order
+let portalPoley = new Poley("portalPoley", "Portal Poley", 0.5)
+let pole = new Poley("pole", "Pole", 10)
+let plantey = new Poley("plantey", "Plantey", 8)
+let magicPoley = new Poley("magicPoley", "Magic Poley", 0.1)
+let goldenPoley = new Poley("goldenPoley", "Golden Poley", 0.6)
+let firePoley = new Poley("firePoley", "Fire Poley", 5.5)
+let enchantedPoley = new Poley("enchantedPoley", "Enchanted Poley", 1)
+let deskPoley = new Poley("deskPoley", "Desk Poley", 10)
+let computerPoley = new Poley("computerPoley", "Computer Poley", 4)
+let classyPoley = new Poley("classyPoley", "Classy Poley", 2.5)
+let calculatorPoley = new Poley("calculatorPoley", "Calculator Poley", 6)
+let bugPoley = new Poley("bugPoley", "Bug Poley", 1)
+let bob = new Poley("bob", "Bob", 13)
+let poley = new Poley("poley", "Poley", totalRarity)
 
-let desk_poley = new Poley("desk poley", 9.9);
-let shiny_desk_poley = new Poley("shiny desk poley", 0.1);
-let deskPoleyCounter = 0;
-let shinyDeskPoleyCounter = 0;
-
-let calculator_poley = new Poley("calculator poley", 5.94);
-let shiny_calculator_poley = new Poley("shiny calculator poley", 0.06)
-let calculatorPoleyCounter = 0;
-let shinyCalculatorPoleyCounter = 0;
-
-let magic_poley = new Poley("magic poley", 0.099);
-let shiny_magic_poley = new Poley("shiny magic poley", 0.001);
-let magicPoleyCounter = 0;
-let shinyMagicPoleyCounter = 0;
-
-let bug_poley = new Poley("bug poley", 0.99);
-let shiny_bug_poley = new Poley("shiny bug poley", 0.01);
-let bugPoleyCounter = 0;
-let shinyBugPoleyCounter = 0;
-
-let pole = new Poley("pole", 9.9);
-let shiny_pole = new Poley("shiny pole", 0.1);
-let poleCounter = 0;
-let shinyPoleCounter = 0;
-
-let bob = new Poley("bob", 12.9);
-let bobCounter = 0;
-
-let poleys = [poley, desk_poley, calculator_poley, magic_poley, bug_poley, pole, shiny_pole, shiny_calculator_poley, shiny_desk_poley, shiny_magic_poley, shiny_poley, shiny_bug_poley, bob]
-
-const music = new Audio("AsianSong.mp3");
-const throwSoundEffect = new Audio("throw.mp3");
-const shinyPoleyEffect = new Audio("shinyPoley.mp3");
-throwSoundEffect.volume = 0.7;
-music.volume = 0.5;
-music.loop = true;
-
-function audio() {
-    music.play();
-}
-
-function throwSound() {
-    throwSoundEffect.play();
-}
-
-function mute() {
-    if (!muted){
-        music.volume = 0;
-        throwSoundEffect.volume = 0.2;
-    } else {
-        if (music.volume != 1){
-            music.volume += 0.5;
-            throwSoundEffect.volume += 0.5;
-        }
-        
-    }
-    muted = !muted;
-}
-
-function volume() {
-    let volumeSlider = $("#volume");
-    console.log(volumeSlider.attr("value"));
-}
-
-function pickPoley(poleyArray) {
-    let poleyArrayRarity = poleyArray.map(function(obj){
-        return obj.rarity;
+function choose() {
+    let rand = Math.random() * 100
+    console.log(rand)
+    let objValues = Object.values(poleysObj)
+    let chosenPoley = objValues.find((x) => {
+        return x.percentChunk[0] >= rand && rand >= x.percentChunk[1] 
     })
-    const totalPercentage = poleyArrayRarity.reduce((acc, val) => acc + val, 0);
-    const randomNumber = Math.random() * totalPercentage;
-    let cumulativePercentage = 0;
-    let selectedObject;
-    for (let i = 0; i < poleyArrayRarity.length; i++) {
-        cumulativePercentage += poleyArrayRarity[i];
-        if (randomNumber <= cumulativePercentage) {
-            selectedObject = poleyArray[i];
-            break;
+    return {poley: chosenPoley, shiny: chosenPoley.shinyPercentChunk[0] >= rand && rand >= chosenPoley.shinyPercentChunk[1] ? true : false}
+}
+
+function sortByText(a, b) {
+    if (a.display < b.display) {
+        return -1;
+    }
+    if (a.display > b.display) {
+        return 1;
+    }
+    return 0;  
+}
+
+function collect() {
+    catchingMusic.play() 
+    throwSfx.playsfx()
+    let chosenPoley = choose()
+    document.querySelector("#poleyimg").setAttribute("src", chosenPoley.poley.texturePath)
+    document.querySelector("#poleygot").innerHTML = "You just got: " + chosenPoley.poley.display
+    if (chosenPoley.shiny) {
+        shinySfx.playsfx()
+        document.querySelector("#poleyimg").setAttribute("src", chosenPoley.poley.shinyTexturePath)
+        document.querySelector("#poleygot").innerHTML = "You just got: " + chosenPoley.poley.shinyDisplay
+        chosenPoley.poley.shinyAmt += 1
+    } else {
+        chosenPoley.poley.amt += 1
+    }
+ 
+    $("#changePoley").attr("disabled", "")
+    updateDisplay(chosenPoley)
+    setTimeout(() => {
+        $("#changePoley").removeAttr("disabled")
+    }, 3000)
+    setTimeout(() => {
+        $("#poleyimg").addClass("pop")
+        setTimeout(() => {
+            $("#poleyimg").removeClass("pop")
+        }, 500);
+    }, 20); // what's callback hell?
+}   
+
+function generateDisplays() {
+    for (i in poleysObj) {
+        i = poleysObj[i]
+        let displayText = `
+            <img src="poleys/${i.name}.png" id="${i.name}Img" class="sill">
+            <br>
+            <span id="${i.name}Counter">???</span>
+        `
+        $("#poleysTitle").after(displayText)
+        let shinyDisplayText = `
+            <img src="poleys/shiny${i.name}.png" id="shiny${i.name}Img" class="sill"
+            <br>
+            <span id="shiny${i.name}Counter">Shiny ???</span>
+        `
+        $("#shinyPoleysTitle").after(shinyDisplayText)
+    }
+}
+
+function updateDisplay(poley) {
+    let name = poley.poley.name
+    if (poley.shiny) {
+        if (poley.poley.shinyAmt >= 1) {
+            $(`#shiny${name}Img`).removeClass("sill")
+            $(`#shiny${name}Counter`).html(`${poley.poley.shinyDisplay}: ${poley.poley.shinyAmt} (${roundBy(poley.poley.shinyRarity, 4)}%)`)
+        }
+    } else {
+        if (poley.poley.amt >= 1) {
+            $(`#${name}Img`).removeClass("sill")
+            $(`#${name}Counter`).html(`${poley.poley.display}: ${poley.poley.amt} (${roundBy(poley.poley.rarity, 4)}%)`)
         }
     }
-    console.log(selectedObject);
-    let poleyMon = selectedObject.name;
-    switch (poleyMon){
-        case "poley":
-            poleyCounter +=1;
-            changePoleyCounter("poleyCounter", "poley", poleyCounter);
-            console.log("poleys: " + poleyCounter)
-            break;
-        case "desk poley":
-            deskPoleyCounter +=1;
-            changePoleyCounter("deskPoleyCounter", "desk poley", deskPoleyCounter);
-            console.log("poleys: "+deskPoleyCounter)
-            break;
-        case "calculator poley":
-            calculatorPoleyCounter +=1;
-            changePoleyCounter("calculatorPoleyCounter", "calculator poley", calculatorPoleyCounter);
-            console.log("poleys: ")
-            break;
-        case "magic poley":
-            magicPoleyCounter +=1;
-            changePoleyCounter("magicPoleyCounter", "magic poley", magicPoleyCounter);
-            console.log("poleys: ")
-            break;
-        case "bug poley":
-            bugPoleyCounter +=1;
-            changePoleyCounter("bugPoleyCounter", "bug poley", bugPoleyCounter);
-            console.log("poleys: ")
-            break;
-        case "pole":
-            poleCounter +=1;
-            changePoleyCounter("poleCounter", "pole", poleCounter);
-            console.log("poleys: ")
-            break;
-        case "shiny poley":
-            shinyPoleyCounter +=1;
-            changePoleyCounter("shinyPoleyCounter", "shiny poley", shinyPoleyCounter)
-            console.log("poleys: ")
-            shinyPoleyEffect.play();
-            break;
-        case "shiny desk poley":
-            shinyDeskPoleyCounter +=1;
-            changePoleyCounter("shinyDeskPoleyCounter", "shiny desk poley", shinyDeskPoleyCounter)
-            console.log("poleys: ")
-            shinyPoleyEffect.play();
-            break;
-        case "shiny calculator poley":
-            shinyCalculatorPoleyCounter +=1;
-            changePoleyCounter("shinyCalculatorPoleyCounter", "shiny calculator poley", shinyCalculatorPoleyCounter);
-            console.log("poleys: ")
-            shinyPoleyEffect.play();
-            break;
-        case "shiny magic poley":
-            shinyMagicPoleyCounter +=1;
-            changePoleyCounter("shinyMagicPoleyCounter", "shiny magic poley", shinyMagicPoleyCounter);
-            console.log("poleys: ")
-            shinyPoleyEffect.play();
-            break;
-        case "shiny bug poley":
-            shinyBugPoleyCounter +=1;
-            changePoleyCounter("shinyBugPoleyCounter", "shiny bug poley", shinyBugPoleyCounter);
-            console.log("poleys: ")
-            shinyPoleyEffect.play();
-            break;
-        case "shiny pole":
-            shinyPoleCounter +=1;
-            changePoleyCounter("shinyPoleCounter", "shiny pole", shinyPoleCounter);
-            console.log("poleys: ")
-            shinyPoleyEffect.play();
-            break;
-        case "bob":
-            bobCounter += 1;
-            changePoleyCounter("bobCounter", "bob", bobCounter);
-            break;
-        default:
-            console.log("error! ln 126, somethin rong :(");
-            
+}
+
+function updateAllDisplays() {
+    for (i in poleysObj) {
+        i = poleysObj[i]
+        updateDisplay({poley: i, shiny: false})
+        updateDisplay({poley: i, shiny: true})
     }
-    return poleyMon;
 }
 
-function changePoleyCounter(poleyCounterName, poleyName, value){
-    $("#" + poleyCounterName).html("<img src='poleys/" + poleyName + ".png'><h5 id='"+ poleyCounterName+"'>"+value+"</h5>")
+function writeSave() {
+    // the most useful one liner in all of JS
+    let normalString = objectMap(poleysObj, (x) => x.amt)
+    let shinyString = objectMap(poleysObj, (x) => x.shinyAmt)
+    let completeString = JSON.stringify({normal: normalString, shiny: shinyString})
+    document.cookie = `save=${btoa(completeString)}; SameSite=Strict; Expires=Tues, 1 Jan 2030 12:00:00 UTC`
 }
 
-function notDisabled(){
-    let changedPoley = $("#changePoley");
-    changedPoley.prop('disabled', false);
-    console.log("not disabled!")
-}
-
-function changePoley() {
-    audio();
-    throwSound();
-    let poleyMon = pickPoley(poleys);
-    let poleyPerson = "poleys/" + poleyMon + ".png";
-    let poleyImg = document.querySelector("#poleyimg");
-    poleyImg.setAttribute("src", poleyPerson);
-    if (poleyPerson.includes("Poley")) {
-        let poleyMon0 = poleyMon.split("");
-        poleyMon0[0] = poleyMon0[0].toUpperCase();
-        poleyMon = poleyMon0.toString();
-        $("#poleygot").html(poleyMon.split(","));
-    } else {
-        let poleyMon0 = poleyMon.split("");
-        poleyMon0[0] = poleyMon0[0].toUpperCase();
-        poleyMon = poleyMon0.toString();
-        $("#poleygot").html(poleyMon.split(","));
+function loadSave() {
+    let save = JSON.parse(atob(document.cookie.substring(5)))
+    for (i in save.normal) {
+        poleysObj[i].amt = save.normal[i]
     }
-    let changedPoley = $("#changePoley");
-    changedPoley.prop('disabled', true);
-    console.log("disabled!");
+    for (i in save.shiny) {
+        poleysObj[i].shinyAmt = save.shiny[i]
+    }
+
+    updateAllDisplays()
 }
 
-function startNotDisabled(){
-    setTimeout(notDisabled, 5000);
+generateDisplays()
+
+if (document.cookie) {
+    loadSave()
 }
+
+setInterval(writeSave, 10000)
+document.onbeforeunload = writeSave
